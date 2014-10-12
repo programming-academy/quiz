45917f9b31da750464abb7e29a0e135b1c5142e6
maxwell1987 <maxwell1987@foxmail.com>
2014-10-12 19:38:56 +0800

    maxwell1987 创建工程，使用springboot1.1.8、springframework4.1.1、jpa、testng6.8.8、tomcat8.0.14、java8、maven

---
+++ src/test/java/org/academy/quiz/ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.academy.quiz;
+
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.annotations.Test;
+
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class ApplicationTests extends AbstractTestNGSpringContextTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
